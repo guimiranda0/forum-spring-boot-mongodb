992c947a81564fe62413d2e734c6bafd1cfd58b4
Guilherme <guimsjesus@gmail.com>
2024-07-20 12:10:03 -0300

    feat: Cria package dto com a classe UserDTO para implementar modelo DTO

--- src/main/java/com/devguimsjesus/workshopmongo/resources/UserResources.java
+++ src/main/java/com/devguimsjesus/workshopmongo/resources/UserResources.java
@@ -1,7 +1,7 @@
 package com.devguimsjesus.workshopmongo.resources;
 
-
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devguimsjesus.workshopmongo.domain.User;
+import com.devguimsjesus.workshopmongo.dto.UserDTO;
 import com.devguimsjesus.workshopmongo.services.UserService;
-
 
 @RestController
 @RequestMapping("/users")
@@ -19,8 +19,9 @@ public class UserResources {
 	@Autowired
 	private UserService service;
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
